48839d1226cd4a6584f741786649ac139e33ef6a
Vicky Adi Firmansyah <vickyadifirmansyah99@gmail.com>
2022-12-18 20:28:53 +0700

    add controller get role by id

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -27,6 +27,38 @@ const getRole = async (_: any, res: Response): Promise<Response> => {
   }
 };
 
+const getRoleById = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const result = await Role.findByPk(id);
+
+    if (!result) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      role: result,
+    });
+  } catch (error: any) {
+    if (error !== null && error instanceof Error) {
+      return res.status(500).send({
+        status: 500,
+        message: error.message,
+        error: error,
+      });
+    }
+    return res.status(500).send({
+      status: 500,
+      message: "Internal server error",
+      error: error,
+    });
+  }
+};
+
 const createRole = async (req: Request, res: Response): Promise<Response> => {
   try {
     const { roleName, active } = req.body;
@@ -56,4 +88,4 @@ const createRole = async (req: Request, res: Response): Promise<Response> => {
   }
 };
 
-export { getRole, createRole };
+export { getRole, getRoleById, createRole };
