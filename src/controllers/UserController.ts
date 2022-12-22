a4b3b45924723fc71ba719e6b1c6b2fe9e687288
Vicky Adi Firmansyah <vickyadifirmansyah99@gmail.com>
2022-12-22 11:46:14 +0700

    feat: login

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { passwordHashing } from "../helpers/PasswordHelper";
+import { passwordHashing, passwordCompare } from "../helpers/PasswordHelper";
 import responseData from "../helpers/Helper";
 import User from "../db/models/User";
 
@@ -22,4 +22,27 @@ const register = async (req: Request, res: Response): Promise<Response> => {
   }
 };
 
-export { register };
+const login = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      return res
+        .status(401)
+        .send(responseData(401, "Unauthorized", null, null));
+    }
+
+    const matched = await passwordCompare(password, user.password);
+    if (!matched) {
+      return res
+        .status(401)
+        .send(responseData(401, "Unauthorized", null, null));
+    }
+
+    return res.status(200).send(responseData(200, "OK", null, user))
+  } catch (error: any) {
+    return res.status(500).send(responseData(500, "", error, null));
+  }
+};
+
+export { register, login };
--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controllers/UserController";
+import { register, login } from "../controllers/UserController";
 import { registerValidation } from "../middlewares/validation/UserValidation";
 
 const route = express.Router();
 
 route.post("/auth/signup", registerValidation, register);
+route.post("/auth/signin", login);
 
 export default route;
